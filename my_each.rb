ec10b129fdcf3d3830519179e0d403bbc2c592ff
+ <mcrea23@aol.com>
2020-11-11 12:46:57 -0600

    commit tree 20cf1d5cbd932725d6ccec79709cd45e2741c080

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(greeting)
+  x = 0
+
+  while x < greeting.length
+        yield greeting[x]
+        x = x + 1
+  end
+  greeting
 end
